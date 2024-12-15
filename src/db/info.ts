b81416a6a58e81a1b0d815ff99cbd65e8561ef1b
Adnan <adnahl@gmail.com>
2024-12-15 19:19:33 -0400

    Add challenge, solution, and process initial data

---
+++ src/db/info.ts
@@ -0,0 +1,25 @@
+export const challenge = {
+  title: 'Challenge',
+  description: [
+    'Find an open source web application or example on building a web application.',
+    "Find one or more areas that are 'good', and one or more areas that are not 'good'.",
+    'You could choose areas that focus on how the UI presents itself, areas that deal with understandable code, areas that are over or under engineered, or any other area that you can contrast approaches.',
+    "Describe why these areas are 'good' or not. For one of the areas that is not 'good', refactor or rewrite it. Describe how you made these changes and why.",
+    'Present this in a format of your choosing so that a panel of interviewers can easily understand What the application is Well written areas Poorly written areas Your rewritten code and a description of it',
+    'We are worried less about completeness and more about your process and approaches to a problem like this.'
+  ],
+  time: '2 weeks',
+  startDate: '2024-12-11',
+  endDate: '2024-12-25',
+  tags: ['web', 'application', 'open source', 'refactor', 'rewrite']
+}
+
+export const solution = {
+  title: 'Solution',
+  description: []
+}
+
+export const process = {
+  title: 'Process',
+  description: []
+}
