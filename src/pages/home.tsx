89204122a31d277f0062afbe4c2c22a773c1f0c5
Adnan <adnahl@gmail.com>
2024-12-15 18:18:11 -0400

    Structure challenge information section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,23 @@
+import Hero from '@/components/header/hero'
+
 export default function HomePage() {
-  return <div>Home</div>
+  return (
+    <section className='space-y-12'>
+      <Hero title='Information about the challenge' />
+
+      <div className='space-y-6'>
+        <h2 className='text-lg text-center tracking-wider'>
+          Challenge Information
+        </h2>
+
+        <div className='space-y-2'>
+          {/* 
+					<ChallengeInfo />
+          <SolutionInfo />
+          <ProcessInfo /> 
+					*/}
+        </div>
+      </div>
+    </section>
+  )
 }
