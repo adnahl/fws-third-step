86a284505d0a280af1c008de55ab310e4768c4ac
Adnan <adnahl@gmail.com>
2024-12-15 22:36:58 -0400

    Update project body paths to reflect new directory structure

--- src/db/projects.ts
+++ src/db/projects.ts
@@ -10,12 +10,12 @@ export const projects: tProject[] = [
     id: '1',
     name: 'Project One',
     description: 'This is project one',
-    bodyPath: '/src/assets/p1.md'
+    bodyPath: '/src/assets/projects/p1.md'
   },
   {
     id: '2',
     name: 'Project Two',
     description: 'This is project two',
-    bodyPath: '/src/assets/p2.md'
+    bodyPath: '/src/assets/projects/p2.md'
   }
 ]
