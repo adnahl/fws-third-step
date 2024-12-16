7b8811e67bc71f0682ddd581b64aade96a8881d5
Adnan <adnahl@gmail.com>
2024-12-15 23:00:35 -0400

    Add GitHub link

--- src/components/header/nav.tsx
+++ src/components/header/nav.tsx
@@ -6,7 +6,7 @@ const idleClass = 'opacity-50 hover:opacity-100'
 
 export default function Nav() {
   return (
-    <nav className='px-4 py-2 border-b border-b-dotted mb-4'>
+    <nav className='px-4 py-2 border-b border-b-dotted mb-4 flex justify-between'>
       <ul className='mx-auto w-full max-w-4xl flex space-x-4'>
         <li>
           <NavLink
@@ -27,6 +27,15 @@ export default function Nav() {
           </li>
         ))}
       </ul>
+      <a
+        title='Github repository'
+        href='https://github.com/adnahl/fws-third-step'
+        target='_blank'
+        rel='noopener noreferrer'
+        className='opacity-50 hover:opacity-100'
+      >
+        Github
+      </a>
     </nav>
   )
 }
